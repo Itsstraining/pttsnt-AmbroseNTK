07a4f636e6e670ccf19fcc6763953d417cb81a12
Nguyễn Tuấn Kiệt <nguyentuankiet23031998@gmail.com>
2022-07-29 16:51:47 +0700

    submit lan 1

--- program.js
+++ program.js
@@ -3,6 +3,8 @@
 function main(input) {
   // Your code goes here
   // Using console.log to answer the question
+  let n = parseInt(input);
+  console.log(n * n + " " + n * 2);
 }
 
 module.exports = main;
